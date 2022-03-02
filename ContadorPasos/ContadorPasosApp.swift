66cafbda8afca0dfee07385a35e1edbeb44707df
= <brainchrisdvlper@gmail.com>
2022-03-02 20:23:38 -0300

    Initial Commit

---
+++ ContadorPasos/ContadorPasosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorPasosApp.swift
+//  ContadorPasos
+//
+//  Created by Carlos Sanchez on 02-03-22.
+//
+
+import SwiftUI
+
+@main
+struct ContadorPasosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContadorPasos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContadorPasos
+//
+//  Created by Carlos Sanchez on 02-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
